3c70c51e125968970e94bd10bcdf50631651b1b2
AreteQin <qinqiaomeng@outlook.com>
2023-02-11 19:47:17 -0500

    launch done

--- imu_publisher.sh
+++ imu_publisher.sh
@@ -2,4 +2,4 @@ source /opt/ros/melodic/setup.bash
 source /home/nvidia/dm-vio-ws/devel/setup.bash
 export ROS_IP=192.168.1.236
 export ROS_MASTER_URI=http://192.168.1.120:11311
-rosrun dmvio_ros /scripts/imu_publisher.py
+rosrun dmvio_ros scripts/imu_publisher.py
