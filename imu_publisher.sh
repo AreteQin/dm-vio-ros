4baf12bbb152ddb1fd69a6680895279207bbb89f
AreteQin <qinqiaomeng@outlook.com>
2023-02-11 19:44:23 -0500

    launch done

--- imu_publisher.sh
+++ imu_publisher.sh
@@ -1,5 +1,5 @@
 source /opt/ros/melodic/setup.bash
-source /home/nvidia/dm_vio_ws/devel/setup.bash
+source /home/nvidia/dm-vio-ws/devel/setup.bash
 export ROS_IP=192.168.1.236
 export ROS_MASTER_URI=http://192.168.1.120:11311
 rosrun dm_vio_ws imu_publisher.py
