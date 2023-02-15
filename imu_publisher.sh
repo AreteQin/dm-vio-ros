eeaa9273c49d73d441fe75338a88c1470778ea31
AreteQin <qinqiaomeng@outlook.com>
2023-02-14 20:04:21 -0500

    pub image and imu in one node

--- imu_publisher.sh
+++ imu_publisher.sh
@@ -2,6 +2,6 @@
 
 source /opt/ros/melodic/setup.bash
 source ~/dm-vio-ws/devel/setup.bash
-export ROS_IP=192.168.1.236
-export ROS_MASTER_URI=http://192.168.1.120:11311
+#export ROS_IP=192.168.1.236
+#export ROS_MASTER_URI=http://192.168.1.120:11311
 rosrun dmvio_ros IMU_publisher.py
