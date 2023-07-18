3a4afef6fbd43b5e8476730f0a0edabe44b3ea37
qin <qinqiaomeng@outlook.com>
2023-07-18 12:11:51 -0400

    qcar removed

--- imu_publisher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/melodic/setup.bash
-source ~/dm-vio-ws/devel/setup.bash
-export ROS_IP=192.168.1.236
-export ROS_MASTER_URI=http://192.168.1.120:11311
-chmod +x /home/nvidia/dm-vio-ws/src/dm-vio-ros/scripts/IMU_publisher.py
-rosrun dmvio_ros IMU_publisher.py
